05ceb646b1105680be83c23405c13750c02006ae
Bian Jiang <borderj@gmail.com>
2011-05-27 13:07:50 +0800

    Handling Web Forms

--- hello/hello.go
+++ hello/hello.go
@@ -1,28 +1,45 @@
 package hello
 
 import (
-    "appengine"
-    "appengine/user"
     "fmt"
     "http"
+    "template"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/", root)
+    http.HandleFunc("/sign", sign)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    u := user.Current(c)
-    if u == nil {
-        url, err := user.LoginURL(c, r.URL.String())
-        if err != nil {
-            http.Error(w, err.String(), http.StatusInternalServerError)
-            return
-        }
-        w.Header().Set("Location", url)
-        w.WriteHeader(http.StatusFound)
-        return
-    }
-    fmt.Fprintf(w, "你好: %v!", u)
+func root(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, guestbookForm)
 }
+
+const guestbookForm = `
+<html>
+  <body>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
+`
+
+func sign(w http.ResponseWriter, r *http.Request) {
+    err := signTemplate.Execute(w, r.FormValue("content"))
+    if err != nil {
+        http.Error(w, err.String(), http.StatusInternalServerError)
+    }
+}
+
+var signTemplate = template.MustParse(signTemplateHTML, nil)
+
+const signTemplateHTML = `
+<html>
+  <body>
+    <p>You wrote:</p>
+    <pre>{@|html}</pre>
+  </body>
+</html>
+`
